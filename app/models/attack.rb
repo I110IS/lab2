57ef59acd9a51be1eb6bcb0fec323e02d0183ab7
soyezequiel <alcayaga147147@gmail.com>
2022-09-22 10:34:32 +0000

    arreglado el problema de que no me deja agregar ataques

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,16 +1,19 @@
 class Attack < ApplicationRecord
-has_many:monster
-has_many:victim
-
-/
+belongs_to:monster
+belongs_to:victim
 validate:eres_muy_asustador_para_esta_victima
 
 
 def eres_muy_asustador_para_esta_victima
-    if (monster.scare_level>5 && victim.birthdate<18)
-        errors.add(:monster.scare_level,message:"tienes un nivel de susto mayor a 5, no puedes atacar a menores de 18")
+    edad=((Time.zone.now - victim.birthdate.to_time) / 1.year.seconds).floor / linea que calcula la edad, es una solucion que encontre en https://medium.com/@craigsheen/calculating-age-in-rails-9bb661f11303   /
+    if (monster.scare_level>5 && edad<18)
         puts(holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa)
+        errors.add(:monster.scare_level,message:"tienes un nivel de susto mayor a 5, no puedes atacar a menores de 18")
+       
     end
+    
+    puts("llegueee")
 end
-/
-end   
+
+end
+
