4643cc815532540cd76c531f51f1bdec840cf36e
soyezequiel <alcayaga147147@gmail.com>
2022-09-22 16:07:50 +0000

    se corrigieron los problemas de las validaciones

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -5,11 +5,9 @@ validate :eres_muy_asustador_para_esta_victima
 
 
 def eres_muy_asustador_para_esta_victima
-    edad = Time.now.year - victim.birthdate.year
+    edad = (Time.now.year - victim.birthdate.year).to_i
     if (monster.scare_level > 5) && (edad<18)
         errors.add(:monster.scare_level,message:"tienes un nivel de susto mayor a 5, no puedes atacar a menores de 18")
     end  
 end
-
 end
-
