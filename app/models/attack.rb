190c6c1ab932df7dc4fabbe054ef8871cf4b03f3
soyezequiel <alcayaga147147@gmail.com>
2022-09-22 15:27:33 +0000

    arreglado el calculo de la edad

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,18 +1,14 @@
 class Attack < ApplicationRecord
-belongs_to:monster
-belongs_to:victim
-validate:eres_muy_asustador_para_esta_victima
+belongs_to :monster
+belongs_to :victim
+validate :eres_muy_asustador_para_esta_victima
 
 
 def eres_muy_asustador_para_esta_victima
-    edad=((Time.zone.now - victim.birthdate.to_time) / 1.year.seconds).floor / linea que calcula la edad, es una solucion que encontre en https://medium.com/@craigsheen/calculating-age-in-rails-9bb661f11303   /
-    if (monster.scare_level>5 && edad<18)
-        puts(holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa)
+    edad = Time.now.year - victim.birthdate.year
+    if (monster.scare_level > 5) && (edad<18)
         errors.add(:monster.scare_level,message:"tienes un nivel de susto mayor a 5, no puedes atacar a menores de 18")
-       
-    end
-    
-    puts("llegueee")
+    end  
 end
 
 end
